ed277bb2b6b85dc331490485a328cfd0a7b5b72d
ssslly <117009020+ssslly@users.noreply.github.com>
2022-10-31 14:41:26 +0800

    Update GetTest.java

--- src/main/java/com/my/git/GetTest.java
+++ src/main/java/com/my/git/GetTest.java
@@ -7,5 +7,6 @@ public class GetTest {
         System.out.println("Hello hotfix");
         System.out.println("Hello ,master");
         System.out.println("Hello hotfix222");
+        System.out.println("Hello hotfix333");
     }
 }
